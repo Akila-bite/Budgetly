697171d830ffc7bd962d3eb0b91c25f092033d20
Nompumelelo <akilaprofvictoria@gmail.com>
2025-06-14 14:36:00 +0200

    defined store and wrapped app in transaction filter provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { store} from "./redux/store"; // Import Redux store
 import { Provider } from "react-redux";
+import { TransactionFilterProvider } from "./context/TransactionFilterContext";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <Provider store={store}> {/* Wrap entire app with Redux store */}
+       <TransactionFilterProvider>
 
      <Router>
             <AppContent />
       </Router>
+
+      </TransactionFilterProvider>
 
         </Provider>
   );
